a2797377eebcc8e14a02ad8027124b76a15dc824
Biswarup Ghoshal <biswarup.ghoshal@gmail.com>
2024-08-20 15:35:26 +0200

    Modified

--- inc/main_slider.php
+++ inc/main_slider.php
@@ -1,4 +1,4 @@
-<section class="pt-3 pb-3 mb-2 card-grid">
+<section class="pt-0 card-grid">
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
@@ -6,73 +6,40 @@
 					<div class="tiny-slider-inner"
 					data-autoplay="true"
 					data-hoverpause="true"
-					data-gutter="1"
+					data-gutter="0"
 					data-arrow="true"
 					data-dots="false"
 					data-items="1">
+<?php 
+$result = mysqli_query($con1,"select *,DATE_FORMAT(CREATE_DATE,'%b %d, %Y') as niceDate from blog");
+$count = 1;
+
+while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){ 
+    
+?>
+
 						<!-- Slide 1 -->
-						<div class="card bg-dark-overlay-3 h-400 h-sm-500 h-md-600 rounded-0" style="background-image:url(https://djbis.s3.ap-south-1.amazonaws.com/assets/images/blog/16by9/05.jpg); background-position: center left; background-size: cover;">
+						<div class="card card-overlay-bottom card-bg-scale h-400 h-sm-500 h-md-600 rounded-0" style="background-image:url(<?php echo $S3_BUCKET_FOLDER.$row['image_1300X732'] ?>); background-position: center left; background-size: cover;">
 							<!-- Card Image overlay -->
 		          <div class="card-img-overlay d-flex align-items-center p-3 p-sm-5"> 
-		            <div class="w-100 my-auto">
-			            <div class="col-md-10 col-lg-7 mx-auto text-center">
+		            <div class="w-100 mt-auto">
+			            <div class="col-md-10 col-lg-7">
 			            	<!-- Card category -->
-			            	<a href="#" class="badge bg-primary mb-2"><i class="fas fa-circle me-2 small fw-bold"></i>Business</a>
+			            	<a href="#" class="badge bg-primary mb-2"><i class="fas fa-circle me-2 small fw-bold"></i><?php echo $row['topic'] ?></a>
 			            	<!-- Card title -->
-			              <h2 class="text-white display-5"><a href="post-single-2.html" class="btn-link text-reset fw-normal">Never underestimate the influence of social media</a></h2>
-			              <p class="text-white">For who thoroughly her boy estimating conviction. Removed demands expense account in outward tedious do.</p>
+			              <h2 class="text-white display-5"><a href="post-single-4.html" class="btn-link text-reset fw-normal"><?php echo $row['title'] ?></a></h2>
+			              <p class="text-white"><?php echo $row['subtitle'] ?></p>
 			              <!-- Card info -->
 										<ul class="nav nav-divider text-white-force align-items-center d-none d-sm-inline-block">
-										  <li class="nav-item">
-										    <div class="nav-link">
-											    <div class="d-flex align-items-center text-white position-relative">
-											      <div class="avatar avatar-sm">
-															<img class="avatar-img rounded-circle" src="https://djbis.s3.ap-south-1.amazonaws.com/assets/images/avatar/14.jpg" alt="avatar">
-														</div>
-														<span class="ms-3">by <a href="#" class="stretched-link text-reset btn-link">Carolyn</a></span>
-													</div>
-												</div>
-										  </li>
-										  <li class="nav-item">Jan 26, 2022</li>
-										  <li class="nav-item"><a href="#" class="btn-link"><i class="far fa-comment-alt me-1"></i> 5 Comments</a></li>
+					
+										  <li class="nav-item"><?php echo $row['niceDate'] ?></li>
+										  <li class="nav-item"> <?php echo $row['duration'] ?></li>
 										</ul>
 			            </div>
 		            </div>
 		          </div>
-						</div>
-						<!-- Slide 2 -->
-						<div class="card bg-dark-overlay-3 h-400 h-sm-500 h-md-600 rounded-0" style="background-image:url(https://djbis.s3.ap-south-1.amazonaws.com/assets/images/blog/16by9/02.jpg); background-position: center left; background-size: cover;">
-							<!-- Card Image overlay -->
-		          <div class="card-img-overlay d-flex align-items-center p-3 p-sm-5"> 
-		            <div class="w-100 my-auto">
-			            <div class="col-md-10 col-lg-7 mx-auto text-center">
-			            	<!-- Card category -->
-			            	<a href="#" class="badge text-bg-danger mb-2"><i class="fas fa-circle me-2 small fw-bold"></i>Lifestyle</a>
-			            	<!-- Card title -->
-			              <h2 class="text-white display-5"><a href="post-single-2.html" class="btn-link text-reset fw-normal">This is why this year will be the year of startups</a></h2>
-			              <p class="text-white">Particular way thoroughly unaffected projection favorable Mrs can be projecting own. </p>
-			              <!-- Card info -->
-										<ul class="nav nav-divider text-white-force align-items-center d-none d-sm-inline-block">
-										  <li class="nav-item">
-										    <div class="nav-link">
-											    <div class="d-flex align-items-center text-white position-relative">
-											      <div class="avatar avatar-sm">
-															<img class="avatar-img rounded-circle" src="https://djbis.s3.ap-south-1.amazonaws.com/assets/images/avatar/10.jpg" alt="avatar">
-														</div>
-														<span class="ms-3">by <a href="#" class="stretched-link text-reset btn-link">Louis</a></span>
-													</div>
-												</div>
-										  </li>
-										  <li class="nav-item">Nov 15, 2022</li>
-										  <li class="nav-item"><a href="#" class="btn-link"><i class="far fa-comment-alt me-1"></i> 3 Comments</a></li>
-										</ul>
-			            </div>
-		            </div>
-		          </div>
-						</div>
-					</div>
-				</div>
+                  <?php } ?>
 			</div>
-		</div>
+		</div> <!-- Row END -->
 	</div>
 </section>
--- inc/variables.php
+++ inc/variables.php
@@ -4,5 +4,6 @@ $_HOST = "localhost";
 $_USER = "djbis";
 $_PASSWD = "Welcome@123#";
 $_DBNAME = "DJBIS";
+$S3_BUCKET_FOLDER ="https://djbis.s3.ap-south-1.amazonaws.com/assets/images/blog/";
 
 ?>
