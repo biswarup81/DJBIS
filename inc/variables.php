c028d60c5615ab65b60b487cfa479a7a340a063d
Biswarup Ghoshal <biswarup.ghoshal@gmail.com>
2024-08-20 17:25:53 +0200

    Update variables.php

--- inc/variables.php
+++ inc/variables.php
@@ -3,7 +3,7 @@
 $_HOST = "localhost";
 $_USER = "djbis";
 $_PASSWD = "Welcome@123#";
-$_DBNAME = "DJBIS";
+$_DBNAME = "djbis";
 $S3_BUCKET_FOLDER ="https://djbis.s3.ap-south-1.amazonaws.com/assets/images/blog/";
 
 ?>
