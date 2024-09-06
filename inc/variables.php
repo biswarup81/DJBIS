238d7f5f8474924911cb352c7a27e1eba42577b7
krishdu <krishdu.p@gmail.com>
2024-09-06 21:10:25 +0530

    added admin panel

--- inc/variables.php
+++ inc/variables.php
@@ -1,9 +1,15 @@
 <?php
 //Define variables
-$_HOST = "localhost";
-$_USER = "djbis";
-$_PASSWD = "Welcome@123#";
+$_HOST = "127.0.0.1";
+$_USER = "root";
+$_PASSWD = "krishnendu";
 $_DBNAME = "djbis";
 $S3_BUCKET_FOLDER ="https://djbis.s3.ap-south-1.amazonaws.com/assets/images/blog/";
+
+// S3 Bucket credentials
+$bucketName = 'djbis';
+$region = 'ap-south-1';
+$accessKey = 'AKIAU5QQEM2FP2FDSVU7';
+$secretKey = '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';
 
 ?>
