200720cfd9f664462ded0cfd52a5f13fd50d5504
Biswarup Ghoshal <biswarup.ghoshal@gmail.com>
2024-11-05 09:32:18 +0100

    Update about-us.php

--- about-us.php
+++ about-us.php
@@ -37,7 +37,7 @@ Inner intro START -->
               <nav class="d-flex justify-content-center" aria-label="breadcrumb">
                 <ol class="breadcrumb breadcrumb-dark breadcrumb-dots mb-0">
                   <li class="breadcrumb-item"><a href="index.php"><i class="bi bi-house me-1"></i> Home</a></li>
-                  <li class="breadcrumb-item active">About DJ BIS</li>
+                  <li class="breadcrumb-item active">About DJ BISWARUP</li>
                 </ol>
               </nav>
             </div>
@@ -58,32 +58,32 @@ About START -->
       <div class="col-xl-9 mx-auto">
         <h2>Story of a Classical Flute player turned to a Techno DJ</h2>
         <p class="lead">
-		Elevate your events with DJ BIS's top-tier DJ services. From weddings and corporate events to nightclubs and private parties, we deliver unforgettable musical experiences.
+		From weddings and corporate events to nightclubs and private parties, I promise to give you an unforgettable musical experiences.
 	        </p>
-        <p>Meet DJ Bis. An Indian Classical Flute player and crazy about Techno Music. Right now, learning how to DJ. What is unique? Mixing the beautiful tunes of Indian Classical music with the energetic beats of Techno. DJ Bis is on a mission to create a new kind of music that brings together the old and the new. Now blending the soulful melodies of classical music with the futuristic sounds of Techno, using the skills to make something fresh and exciting for everyone to enjoy.</p>
+        <p>An Indian Classical Flute player and crazy about Techno Music. What is unique? Mixing the Indian Classical music with the energetic beats of Techno. A brand new kind of music that brings together the old and the new. Now blending the soulful melodies of classical music with the futuristic sounds of Techno.</p>
         <h3 class="mt-4">DJ Services curated to your taste</h3>
         <ul>
           <li><b>Weddings: </b>Personalized playlists for your special day.</li>
-          <li><b>Corporate Events: </b>DJ BIS combines professionalism, experience, and a client-centric approach to deliver exceptional DJ services for corporate events. Our commitment to punctuality, reliability, and a polished presentation ensures a seamless and memorable experience. With years of expertise in catering to diverse corporate functions, we customize our offerings to meet your specific needs and preferences. From tailored playlists and state-of-the-art sound and lighting to emcee services and interactive entertainment, we work closely with you to bring your vision to life and make your event a resounding success.</li>
-          <li><b>Private Parties: </b>When it comes to private parties, [DJ Name] is your go-to source for turning any gathering into an unforgettable celebration. Whether you're planning a birthday bash, anniversary celebration, graduation party, or any other special occasion, our DJ services are designed to elevate the atmosphere and keep your guests entertained from start to finish.</li>
-          <li><b>Club Nights: </b>specialize in creating electrifying nightclub experiences that keep the dance floor packed and the energy levels soaring. With a deep understanding of club culture and a talent for reading the crowd, we bring dynamic and unforgettable performances that resonate with partygoers and club owners alike. Whether it's a high-profile venue, an exclusive members-only club, or a trendy nightlife hotspot, our DJ services are designed to elevate your club nights to new heights.</li>
+          <li><b>Corporate Events: </b>With professionalism, experience, and knoweldge I customize playlist to meet your specific needs and preferences. </li>
+          <li><b>Private Parties: </b>When it comes to private parties, I guarantee an unforgettable celebration. Whether you're planning a birthday bash, anniversary celebration, graduation party, or any other special occasion, with my skill, I can elevate the atmosphere.</li>
+          <li><b>Club Nights: </b>specialize in creating electrifying nightclub experiences that keep the dance floor packed and the energy levels soaring. It's a special experience with Flute and DJ console with Techno beats.</li>
         </ul>
         
         <!-- Service START -->
-        <h3 class="mb-3 mt-5">What we do</h3>
+        <h3 class="mb-3 mt-5">What I do</h3>
         <div class="row">
           <!-- Service item-->
           <div class="col-md-6 col-lg-4 mb-4">
             <img class="rounded" src="<?php echo $S3_BUCKET_FOLDER;?>coporate-events.jpg" alt="Card image">
             <h4 class="mt-3">Corporate Events</h4>
-            <p>Every event is unique, and we pride ourselves on our ability to customize our services to meet your specific needs and preferences.
+            <p>Every event is unique, I can customize the DJ with Flute and Saxophone Blues to meet your specific needs and preferences.
             </p>
           </div>
           <!-- Service item-->
           <div class="col-md-6 col-lg-4 mb-4">
             <img class="rounded" src="<?php echo $S3_BUCKET_FOLDER;?>private-party.jpg" alt="Card image">
             <h4 class="mt-3">Private Parties</h4>
-            <p>A playlist that reflects your personal taste and the vibe of your party. From chart-topping hits to timeless classics, we ensure the music suits the occasion and keeps everyone dancing.</p>
+            <p>A playlist that reflects your personal taste and the vibe of your party. From chart-topping hits to timeless classics, I ensure the music suits the occasion and keeps everyone dancing.</p>
           </div>
           <!-- Service item-->
           <div class="col-md-6 col-lg-4 mb-4">
